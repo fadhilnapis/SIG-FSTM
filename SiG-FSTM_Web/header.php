d87f447896a80db70abe62e439cc8763a6468f68
Fadhil Napis <fadzbobby95@gmail.com>
2017-05-02 11:09:51 +0800

    fix source path and add include folder

---
+++ SiG-FSTM_Web/header.php
@@ -0,0 +1,8 @@
+<head>
+    <title>SIG FSTM</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link href="https://fonts.googleapis.com/css?family=Abel" rel="stylesheet">
+    <link rel="stylesheet" href="css/material.min.css">
+    <link rel="stylesheet" href="css/sig.css">
+</head>
